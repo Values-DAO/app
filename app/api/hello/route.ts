50b6ccdfb7e3eb20389c2128de36700f034914c2
Siddesh <siddeshsankhya@gmail.com>
2024-04-28 10:22:39 +0530

    remove test api

--- app/api/hello/route.ts
+++
@@ -1,7 +0,0 @@
-import {generateInviteCodes} from "@/lib/generate-invite-code";
-import {NextResponse} from "next/server";
-
-export function GET() {
-  const codes = generateInviteCodes(new Date());
-  return NextResponse.json(codes);
-}
