4146e2b020dc0602ef4f033b9b7ade8a0a789cc9
Siddesh <siddeshsankhya@gmail.com>
2024-05-02 23:16:11 +0530

    add farcon page

---
+++ app/api/farcon/route.ts
@@ -0,0 +1,71 @@
+import connectToDatabase from "@/lib/connect-to-db";
+import {FARCON_TICKETS} from "@/lib/constants";
+import {fetchSocialsBulk} from "@/lib/fetch-farcaster-userinfo";
+import getNFTHolders from "@/lib/get-nft-holders";
+import Farcon from "@/models/farcon";
+import {headers} from "next/headers";
+import {NextResponse} from "next/server";
+
+export const POST = async (req: any) => {
+  const headersList = headers();
+
+  const apiKey = headersList.get("x-api-key");
+  if (!apiKey) {
+    return NextResponse.json({
+      error: "Missing API key",
+      status: 401,
+    });
+  }
+
+  if (apiKey !== process.env.MASTER_API_KEY) {
+    return NextResponse.json({
+      error: "Invalid API key",
+      status: 401,
+    });
+  }
+  const fetchFarconSummitHolders = async () => {
+    const holders = [];
+    const summitPassHolders = await getNFTHolders(FARCON_TICKETS.summit);
+    holders.push(...summitPassHolders);
+
+    const communityPassHolders = await getNFTHolders(FARCON_TICKETS.community);
+    holders.push(...communityPassHolders);
+
+    const vibesPassHolders = await getNFTHolders(FARCON_TICKETS.vibes);
+    holders.push(...vibesPassHolders);
+    return holders;
+  };
+  try {
+    await connectToDatabase();
+    const holders = await fetchFarconSummitHolders();
+
+    const batchProcess = async (holders: string[]) => {
+      const batchSize = 50;
+      const socials = [];
+
+      for (let i = 0; i < holders.length; i += batchSize) {
+        const batch = holders.slice(i, i + batchSize);
+        const batchSocials = await fetchSocialsBulk(batch);
+        socials.push(...batchSocials);
+      }
+
+      return socials;
+    };
+
+    const socials = await batchProcess(holders);
+
+    const users = await Farcon.insertMany(socials);
+    return NextResponse.json({count: users.length, users});
+  } catch (error) {
+    console.error("Error fetching farcon holders:", error);
+    return NextResponse.json({error: error}, {status: 500});
+  }
+};
+export const GET = async (req: any) => {
+  try {
+    const users = await Farcon.find({});
+    return NextResponse.json({total: users.length, users});
+  } catch (error) {
+    return NextResponse.json({error: error}, {status: 500});
+  }
+};
---
+++ app/farcon-aligned/page.tsx
@@ -0,0 +1,150 @@
+"use client";
+import {Avatar, AvatarFallback, AvatarImage} from "@/components/ui/avatar";
+import {Badge} from "@/components/ui/badge";
+import {Button} from "@/components/ui/button";
+import {Input} from "@/components/ui/input";
+import {usePrivy} from "@privy-io/react-auth";
+
+import axios from "axios";
+
+import React, {useEffect, useState} from "react";
+interface FarconPassHolder {
+  username: string;
+  address: string[];
+  image: string;
+  fid: string;
+}
+const FarconPage = () => {
+  const [farconPassHolders, setFarconPassHolders] = useState<
+    FarconPassHolder[]
+  >([]);
+  const [searchterm, setSearchterm] = useState<string>("");
+  const [filteredUsers, setFilteredUsers] = useState<FarconPassHolder[]>([]);
+  const [alignment, setAlignment] = useState<any>({
+    index: undefined,
+    alignment: undefined,
+  });
+  const {user} = usePrivy();
+  const analyseAlignment = async (fid: string) => {
+    const response = await axios.get(
+      `api/alignment?email=${user?.email?.address}&fid=${fid}`
+    );
+
+    if (response.data.alignment) {
+      setAlignment({
+        index: fid,
+        alignment: response.data.alignment,
+      });
+    } else {
+      setAlignment({
+        index: fid,
+        alignment: "User has not minted any values.",
+      });
+    }
+  };
+  useEffect(() => {
+    function searchByUsername(searchTerm: string) {
+      if (searchTerm.length === 0) {
+        setFilteredUsers(farconPassHolders);
+      } else {
+        setFilteredUsers(
+          farconPassHolders.filter((user: FarconPassHolder) =>
+            user.username.toLowerCase().includes(searchTerm.toLowerCase())
+          )
+        );
+      }
+    }
+    searchByUsername(searchterm);
+  }, [searchterm, farconPassHolders]);
+  useEffect(() => {
+    const getHolders = async () => {
+      const holders = await axios.get("/api/farcon");
+
+      if (holders.data) {
+        setFarconPassHolders(holders.data.users);
+      }
+    };
+    getHolders();
+  }, []);
+  return (
+    <div className="flex flex-col gap-4 p-4">
+      <div className="flex flex-col gap-2 ">
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
+          The Farcon Pass Holders
+        </h2>
+        <Input
+          placeholder="Search for a Farcon Pass Holder"
+          type="text"
+          inputMode="text"
+          value={searchterm}
+          onChange={(e) => {
+            setSearchterm(e.target.value);
+          }}
+          className="border-white/10 bg-white/10 text-white/90"
+        />
+      </div>
+
+      {filteredUsers &&
+        filteredUsers.map((farconPassHolder, index) => {
+          return (
+            <div
+              className="px-4 py-2 bg-gray-400/30 flex flex-row gap-4 items-center rounded-md"
+              key={index}
+            >
+              <Avatar>
+                <AvatarImage src={farconPassHolder.image} />
+                <AvatarFallback>{farconPassHolder.username}</AvatarFallback>
+              </Avatar>
+
+              <div className="flex-grow gap-2 max-w-[60%] md:max-w-[90%]">
+                <p className="scroll-m-20 text-2xl font-semibold tracking-tight text-primary">
+                  {farconPassHolder.username}
+                </p>
+
+                <div className="flex flex-row gap-2 flex-wrap my-1">
+                  {farconPassHolder.address.length > 0 &&
+                    farconPassHolder.address.map((address) => (
+                      <Badge key={address} variant="secondary">
+                        {address.slice(0, 6)}...{address.slice(-4)}
+                      </Badge>
+                    ))}
+                </div>
+              </div>
+              {alignment.index === farconPassHolder.fid ? (
+                <Badge
+                  variant={
+                    !isNaN(parseFloat(alignment.alignment)) &&
+                    isFinite(alignment.alignment)
+                      ? "default"
+                      : "destructive"
+                  }
+                  className={`text-md md:text-xl w-48 md:min-w-48 md:w-[fit-content] py-2 text-center justify-center text-white ${
+                    !isNaN(parseFloat(alignment.alignment)) &&
+                    isFinite(alignment.alignment)
+                      ? "bg-green-500/60"
+                      : "destructive"
+                  } `}
+                >
+                  {isNaN(parseFloat(alignment.alignment))
+                    ? "User hasn't minted any values"
+                    : alignment.alignment + "%"}
+                </Badge>
+              ) : (
+                <Button
+                  variant={"default"}
+                  className="w-48"
+                  onClick={() => {
+                    analyseAlignment(farconPassHolder.fid);
+                  }}
+                >
+                  Find Alignment
+                </Button>
+              )}
+            </div>
+          );
+        })}
+    </div>
+  );
+};
+
+export default FarconPage;
