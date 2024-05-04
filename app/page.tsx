a540b88fcbdbe30dbf8a82453869105f394b37db
Siddesh <siddeshsankhya@gmail.com>
2024-05-05 00:28:15 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
           ) : (
             <div className="flex flex-col items-center px-6 mt-[40%] md:mt-[15%]">
               <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">
-                Collect you Values!
+                Collect your Values!
               </h1>
               <p className="leading-7 [&:not(:first-child)]:mt-6 text-center">
                 Sign up and mint your values onchain.
