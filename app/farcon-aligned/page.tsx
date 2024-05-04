79778dcfbad11e9aa9bf2d5b521a02191f9d79cb
Siddesh <siddeshsankhya@gmail.com>
2024-05-05 01:06:14 +0530

    bug fix

--- app/farcon-aligned/page.tsx
+++ app/farcon-aligned/page.tsx
@@ -153,7 +153,8 @@ const FarconPage = () => {
           <p className="font-semibold text-lg">Most ||aligned</p>
           <div className="flex flex-col md:flex-row gap-2 justify-evenly w-[100%] m-auto">
             {mostAligned.map((farconPassHolder, index) => {
-              if(farconPassHolder.fid === String(user?.farcaster?.fid ||  0)) return null;
+              if (farconPassHolder.fid === String(user?.farcaster?.fid || 0))
+                return;
               return (
                 <Card key={index} className="w-full bg-gray-400/30">
                   <CardHeader>
@@ -220,7 +221,8 @@ const FarconPage = () => {
         filteredUsers
           .slice(mostAligned?.length ?? 0)
           .map((farconPassHolder, index) => {
-            if
+            if (farconPassHolder.fid === String(user?.farcaster?.fid || 0))
+              return;
             return (
               <Card
                 className="px-4 py-2 flex flex-row gap-4 items-center rounded-md"
