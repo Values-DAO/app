e4a67fc93400898a8ca3ae6321a54f496b6ea073
Siddesh <siddeshsankhya@gmail.com>
2024-06-01 13:22:05 +0530

    style(projects):cosmetic change

--- components/projects.tsx
+++ components/projects.tsx
@@ -36,8 +36,13 @@ const Projects = ({limit, style}: {limit?: number; style?: string}) => {
   return (
     <div className="p-4">
       <h2 className="scroll-m-20 text-center border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0 text-muted-foreground mb-2">
-        || community
+        || community mint
       </h2>
+
+      <p className="text-2xl font-semibold text-center mt-4 mb-4">
+        Coming Soon
+      </p>
+
       <Table>
         <TableHeader>
           <TableRow>
@@ -50,28 +55,17 @@ const Projects = ({limit, style}: {limit?: number; style?: string}) => {
           {projects.map((project) => (
             <TableRow
               key={project.id}
-              className="cursor-pointer"
-              onClick={() => {
-                window.location.href = `/community/${project.name.replace(
-                  / /g,
-                  "-"
-                )}-${project.id}`;
-              }}
+              className="cursor-pointer relative group"
             >
-              <Link
-                href={`/community/${project.name.replace(/ /g, "-")}-${
-                  project.id
-                }`}
-                key={project.id}
-              >
-                <TableCell>{project.name}</TableCell>
-              </Link>
+              <TableCell>
+                <div className="relative">{project.name}</div>
+              </TableCell>
               <TableCell>
                 <div className="flex flex-col md:grid md:grid-cols-4 gap-2">
                   {project.values.map((value: string, index: number) => (
                     <Badge
                       key={index}
-                      variant={"default"}
+                      variant="default"
                       className="rounded-sm text-[18px] bg-transparent border border-primary text-primary"
                     >
                       {value}
