e57df7c60f33712038f818a636692fe88f8e91db
Siddesh <siddeshsankhya@gmail.com>
2024-06-29 13:25:30 +0530

    landing page loader screen

--- app/page.tsx
+++ app/page.tsx
@@ -28,22 +28,30 @@ export default function Home() {
           {userInfo && <HomeComponent />}
         </>
       ) : (
-        <div className="flex flex-col items-center px-6 mt-[40%] md:mt-[15%]">
-          <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">
-            Collect your Values!
-          </h1>
-          <p className="leading-7 [&:not(:first-child)]:mt-6 text-center">
-            Sign up and mint your values onchain.
-          </p>
-          <Button
-            variant="default"
-            onClick={login}
-            disabled={!ready || authenticated}
-            className="my-4"
-          >
-            Get Started
-          </Button>
-        </div>
+        <>
+          {ready ? (
+            <div className="flex flex-col items-center px-6 mt-[40%] md:mt-[15%]">
+              <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-6xl">
+                Collect your Values!
+              </h1>
+              <p className="leading-7 [&:not(:first-child)]:mt-6 text-center">
+                Sign up and mint your values onchain.
+              </p>
+              <Button
+                variant="default"
+                onClick={login}
+                disabled={!ready || authenticated}
+                className="my-4"
+              >
+                Get Started
+              </Button>
+            </div>
+          ) : (
+            <div className="flex items-center justify-center h-[60vh]">
+              <div className="animate-spin rounded-full h-14 w-14 border-t-2 border-b-2 border-primary"></div>
+            </div>
+          )}
+        </>
       )}
 
       {}
