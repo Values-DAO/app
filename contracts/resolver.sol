48215a94fd5329c2aaa77ccd13ba7f8b9d1f3de9
Ewanretor Giwa-Okugbe <ewanretorokugbe@gmail.com>
2024-08-16 00:31:12 +0100

    included public access right to file

--- contracts/resolver.sol
+++ contracts/resolver.sol
@@ -6,6 +6,7 @@ import { IEAS, Attestation } from "@ethereum-attestation-service/eas-contracts/c
 
 contract Resolver is SchemaResolver {
     address private _targetAttester;
+    bool private _access;
 
     constructor(IEAS eas, address targetAttester) SchemaResolver(eas) {
         _targetAttester = targetAttester;
@@ -19,8 +20,20 @@ contract Resolver is SchemaResolver {
         _targetAttester = targetAttester;
     }
 
+    function setAccess(bool access) public {
+        require(
+            msg.sender == _targetAttester,
+            "Only the previous attester can change the access value."
+        );
+        _access = access;
+    }
+
     function onAttest(Attestation calldata attestation, uint256 /*value*/) internal view override returns (bool) {
-        return attestation.attester == _targetAttester;
+        if (_access == true) {
+            return true;
+        } else {
+            return attestation.attester == _targetAttester;
+        }
     }
 
     function onRevoke(Attestation calldata /*attestation*/, uint256 /*value*/) internal pure override returns (bool) {
