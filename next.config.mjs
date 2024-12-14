b7db84b3a6f807429d61d7a4becb37c68da4cb25
Mohit Kamlesh Panchal <23110208@iitgn.ac.in>
2024-12-14 19:54:55 +0530

    fix prod errors

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,5 @@
-import {withSentryConfig} from "@sentry/nextjs";
+import { withSentryConfig } from "@sentry/nextjs";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
@@ -19,41 +20,35 @@ const nextConfig = {
 };
 
 export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://github.com/getsentry/sentry-webpack-plugin#options
+  // Sentry configuration
+  org: "values-dao",
+  project: "javascript-nextjs-ba",
 
-org: "values-dao",
-project: "javascript-nextjs-ba",
+  // Only print logs for uploading source maps in CI
+  silent: !process.env.CI,
 
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
+  // Upload a larger set of source maps for prettier stack traces (increases build time)
+  widenClientFileUpload: true,
 
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+  // Automatically annotate React components to show their full name in breadcrumbs and session replay
+  reactComponentAnnotation: {
+    enabled: true,
+  },
 
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
+  // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+  tunnelRoute: "/monitoring",
 
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
+  // Hides source maps from generated client bundles (to protect your source code)
+  hideSourceMaps: true,
 
-// Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-tunnelRoute: "/monitoring",
+  // Automatically tree-shake Sentry logger statements to reduce bundle size
+  disableLogger: true,
 
-// Hides source maps from generated client bundles
-hideSourceMaps: true,
+  // Option to delete source maps after uploading to Sentry
+  deleteSourceMapsAfterUpload: true, // This will delete source maps after upload
 
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
+  // Option to disable source map generation entirely
+  sourcemaps: {
+    disable: true, // Uncomment this if you want to completely disable source maps
+  },
 });
